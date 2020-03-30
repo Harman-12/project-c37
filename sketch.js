c3a97f2d747dc1838ab5d7296619ff5fec3288c8
Harmanjot Singh <s.harmanjot2007@gmail.com>
2020-03-30 14:06:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+var canvas;
+var bg;
+
+var name;
+var userCount;
+
+var form, paint, user;
+var database;
+
+var drawing = [];
+
+var paintState=0;
+
+var weight = 4;
+
+let ele;
+
+let colorPicker;
+
+function preload(){
+bg = loadImage("bg.jpg");
+}
+
+function setup(){
+    var canvas = createCanvas(1900,1090);
+    database = firebase.database();
+  paint = new Paint();
+  paint.getState();
+  paint.start();
+
+  
+
+  ele = createAudio('The-Avengers-Theme-Song.mp3');
+
+  // here we set the element to autoplay
+  // The element will play as soon
+  // as it is able to do so.
+  ele.autoplay(true);
+
+  //let colorPicker;
+
+ 
+  colorPicker = createColorPicker('#ed225d');
+  colorPicker.position(100, 600);
+
+
+
+}
+  
+
+
+function draw(){
+    background(bg);
+    
+    if(mouseIsPressed){
+      var point = {
+        x: mouseX,
+        y: mouseY
+      };
+      drawing.push(point);
+    }
+
+  fill("black");
+   text("Press Left and Right Arroy key to change strokeWeight", 50, 200);
+   text("strokeColor",100, 580);
+
+    beginShape();
+    stroke(colorPicker.color());
+    strokeWeight(weight);
+    noFill();
+    for (var i =0; i<drawing.length; i++){
+      vertex(drawing[i].x, drawing[i].y)
+    }
+    endShape();
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    weight = weight + 1;
+  } else if (keyCode === RIGHT_ARROW) {
+    weight = weight - 1;
+  }
+}
+
+function clearCanvas(){
+  drawing=[];
+}
